25c79939c20541c28bfba1d2704f77f08d266519
B1gels <fikar992@gmail.com>
2025-10-17 09:19:09 +0800

    menghapus file tidak penting

--- hash.js
+++
@@ -1,6 +0,0 @@
-import bcrypt from "bcrypt";
-
-const password = "lajarusadmin123"; // password yang kamu mau
-const hash = await bcrypt.hash(password, 10);
-
-console.log("Hashed password:", hash);
